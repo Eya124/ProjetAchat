ae97fb8c42b12a050b21c50360cc0c9440780b90
Firas <firasbenali65@gmail.com>
2023-04-09 13:34:59 +0100

    Firas-addCategorieProduit

--- src/main/java/com/esprit/examen/controllers/ProjectController.java
+++ src/main/java/com/esprit/examen/controllers/ProjectController.java
@@ -6,5 +6,9 @@ import java.util.List;
 @RestController
 
 public class ProjectController {
-	
+	@PostMapping
+	public CategorieProduit addCategorieProduit(@RequestBody CategorieProduit cp) {
+		CategorieProduit categorieProduit = categorieProduitService.addCategorieProduit(cp);
+		return categorieProduit;
+	}
 }
