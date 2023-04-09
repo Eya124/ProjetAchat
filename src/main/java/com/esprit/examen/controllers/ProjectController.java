05db044d85987ea8001e3a66b341eef835b3bc1e
IbrahimEsprit <ibrahim.barkallah@esprit.tn>
2023-04-09 13:15:36 +0100

    add retrievesecteuractivite function

--- src/main/java/com/esprit/examen/controllers/ProjectController.java
+++ src/main/java/com/esprit/examen/controllers/ProjectController.java
@@ -6,5 +6,10 @@ import java.util.List;
 @RestController
 
 public class ProjectController {
+
+    @GetMapping("/{secteurActivite-id}")
+	public SecteurActivite retrieveSecteurActivite(@PathVariable("secteurActivite-id") Long secteurActiviteId) {
+		return secteurActiviteService.retrieveSecteurActivite(secteurActiviteId);
+	}
 	
 }
