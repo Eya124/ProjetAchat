249bf48e23130088ec563a5451440d097e4b69ae
Eya124 <eya.trabelsi.1@esprit.tn>
2023-04-09 14:22:01 +0200

    getProduits

--- src/main/java/com/esprit/examen/controllers/ProjectController.java
+++ src/main/java/com/esprit/examen/controllers/ProjectController.java
@@ -6,5 +6,10 @@ import java.util.List;
 @RestController
 
 public class ProjectController {
+    @GetMapping
+	public List<Produit> getProduits() {
+		return produitService.retrieveAllProduits();
+	}
+    
 	
 }
