26e93ab271dea824ce3fb9b0f4fbef2a52f6d3a1
ayed <78506293+ayed-hachich@users.noreply.github.com>
2023-04-09 13:19:30 +0100

    addNewFunction

--- src/main/java/com/esprit/examen/controllers/ProjectController.java
+++ src/main/java/com/esprit/examen/controllers/ProjectController.java
@@ -6,5 +6,11 @@ import java.util.List;
 @RestController
 
 public class ProjectController {
+    
+    @GetMapping("/{facture-id}")
+
+    public Facture retrieveFacture(@PathVariable("facture-id") Long factureId) {
+        return factureService.retrieveFacture(factureId);
+    }
 	
 }
