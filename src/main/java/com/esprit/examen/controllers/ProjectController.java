b6559548c5a8c12529689c78790f270d4e1b728c
YoussefWeslatiEsprit <weslati.youssef@esprit.tn>
2023-04-09 13:31:02 +0100

    getStocks

--- src/main/java/com/esprit/examen/controllers/ProjectController.java
+++ src/main/java/com/esprit/examen/controllers/ProjectController.java
@@ -6,5 +6,8 @@ import java.util.List;
 @RestController
 
 public class ProjectController {
-	
+	@GetMapping("/retrieve-all-stocks")
+	public List<Stock> getStocks() {
+		return stockService.retrieveAllStocks();
+	}
 }
