71463dd5c6c89a5ae80b65b1251cd313b2ce7ea5
Firas <firasbenali65@gmail.com>
2023-04-15 02:21:30 +0100

    Firas-lesfocntionsDeCategorieProduitController

--- src/main/java/com/esprit/examen/controllers/ProjectController.java
+++ src/main/java/com/esprit/examen/controllers/ProjectController.java
@@ -6,21 +6,50 @@ import java.util.List;
 @RestController
 
 public class ProjectController {
+
+    private final ICategorieProduitService categorieProduitService;
+	
+	@GetMapping
+	public List<CategorieProduit> getCategorieProduit() {
+		List<CategorieProduit> list = categorieProduitService.retrieveAllCategorieProduits();
+		return list;
+	}
+
+	@GetMapping("/{categorieProduit-id}")
+	public CategorieProduit retrieveCategorieProduit(@PathVariable("categorieProduit-id") Long categorieProduitId) {
+		return categorieProduitService.retrieveCategorieProduit(categorieProduitId);
+	}
+
+	@PostMapping
+	public CategorieProduit addCategorieProduit(@RequestBody CategorieProduit cp) {
+		CategorieProduit categorieProduit = categorieProduitService.addCategorieProduit(cp);
+		return categorieProduit;
+	}
+
+    @DeleteMapping("{categorieProduit-id}")
+	public void removeCategorieProduit(@PathVariable("categorieProduit-id") Long categorieProduitId) {
+		categorieProduitService.deleteCategorieProduit(categorieProduitId);
+	}
+
+	@PutMapping
+	public CategorieProduit modifyCategorieProduit(@RequestBody CategorieProduit categorieProduit) {
+		return categorieProduitService.updateCategorieProduit(categorieProduit);
+	}
     public Reglement addReglement(@RequestBody Reglement r) {
         Reglement reglement = reglementService.addReglement(r);
         return reglement;
-
+    }
 	@GetMapping("/retrieve-all-stocks")
 	public List<Stock> getStocks() {
 		return stockService.retrieveAllStocks();
 	}
+
     @GetMapping
 	public List<Produit> getProduits() {
 		return produitService.retrieveAllProduits();
 	}
     
     @GetMapping("/{facture-id}")
-
     public Facture retrieveFacture(@PathVariable("facture-id") Long factureId) {
         return factureService.retrieveFacture(factureId);
     }
@@ -30,19 +59,4 @@ public class ProjectController {
 		return secteurActiviteService.retrieveSecteurActivite(secteurActiviteId);
 	}
 
-	
-}
-package com.esprit.examen.controllers;
-
-import java.util.List;
-
-
-@RestController
-
-public class ProjectController {
-	@PostMapping
-	public CategorieProduit addCategorieProduit(@RequestBody CategorieProduit cp) {
-		CategorieProduit categorieProduit = categorieProduitService.addCategorieProduit(cp);
-		return categorieProduit;
-	}
 }
