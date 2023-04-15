0e209e2caa57642965afa54036d2a35313f61695
Firas <firasbenali65@gmail.com>
2023-04-15 01:23:46 +0100

    Firas-AllMethod-Categorie-Produit-Controller-In-ProjectController

--- src/main/java/com/esprit/examen/controllers/ProjectController.java
+++ src/main/java/com/esprit/examen/controllers/ProjectController.java
@@ -6,6 +6,40 @@ import java.util.List;
 @RestController
 
 public class ProjectController {
+    public Reglement addReglement(@RequestBody Reglement r) {
+        Reglement reglement = reglementService.addReglement(r);
+        return reglement;
+
+	@GetMapping("/retrieve-all-stocks")
+	public List<Stock> getStocks() {
+		return stockService.retrieveAllStocks();
+	}
+    @GetMapping
+	public List<Produit> getProduits() {
+		return produitService.retrieveAllProduits();
+	}
+    
+    @GetMapping("/{facture-id}")
+
+    public Facture retrieveFacture(@PathVariable("facture-id") Long factureId) {
+        return factureService.retrieveFacture(factureId);
+    }
+
+    @GetMapping("/{secteurActivite-id}")
+	public SecteurActivite retrieveSecteurActivite(@PathVariable("secteurActivite-id") Long secteurActiviteId) {
+		return secteurActiviteService.retrieveSecteurActivite(secteurActiviteId);
+	}
+
+	
+}
+package com.esprit.examen.controllers;
+
+import java.util.List;
+
+
+@RestController
+
+public class ProjectController {
 	@PostMapping
 	public CategorieProduit addCategorieProduit(@RequestBody CategorieProduit cp) {
 		CategorieProduit categorieProduit = categorieProduitService.addCategorieProduit(cp);
